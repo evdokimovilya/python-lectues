367fd4639d4141850cb5b972e6743cf57102686c
evdokimovilya <evdokimovilya@yandex.ru>
2025-06-27 13:56:11 +0500

    add json files

---
+++ pages/5_Работа с json.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+with open("content/Работа с JSON файлами.md", "r", encoding="utf-8") as f:
+    content = f.read()
+    
+st.markdown(content, unsafe_allow_html=True)
--- главная.py
+++ главная.py
@@ -9,5 +9,7 @@ st.page_link("pages/1_Переменные.py", label="1.Переменные")
 st.page_link("pages/2_Функция Input. Типы данных.py", label="2.Input. Типы данных")
 st.page_link("pages/3_Условные операторы.py", label="3.Условные операторы")
 st.page_link("pages/4_Работа с файлами.py", label="4.Работа с файлами")
+st.page_link("pages/5_Работа с json.py", label="5.Работа с JSON файлами")
+
 
 
