8b75009070567fb9dabf717fadef4fda3d2140f2
evdokimovilya <evdokimovilya@yandex.ru>
2025-07-09 12:59:10 +0500

    add for while

--- главная.py
+++ главная.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Лекции по Python", layout="centered")
 st.title("📘 Лекции по программированию на Python")
 st.markdown("Выберите лекцию из списка ниже либо в меню слева.")
 
-files = sorted([f for f in os.listdir('pages') if f.endswith(".py")])
+files = sorted([f for f in os.listdir('pages') if f.endswith(".py")], key=lambda x: int(x.split('_')[0]))
 for file in files:
     st.page_link(f"pages/{file}", label=file.replace('_', '.')[:-3])
 
