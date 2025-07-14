62bf78ac7084c01f4274291ffea09b74c42b2aa3
evdokimovilya <evdokimovilya@yandex.ru>
2025-07-14 17:44:47 +0500

    add sql

--- pages/14_Базы данных. Нормальные формы.py
+++ pages/14_Базы данных. Нормальные формы.py
@@ -24,7 +24,7 @@ def convert_backticks_to_bold(text):
     return "".join(parts)
 
 # Основной код страницы
-with open(r"content/14_Yandex API.md", "r", encoding="utf-8") as f:
+with open(r"content/14_Базы данных. Нормальные формы.md", "r", encoding="utf-8") as f:
     content = f.read()
     
 rendered = convert_backticks_to_bold(content)
