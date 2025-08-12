d473b74a8c441b9566eb64cb3a2feddc64955c9a
evdokimovilya <evdokimovilya@yandex.ru>
2025-08-12 12:17:04 +0500

    add oop 1

--- pages/11_Телеграм боты с нуля.py
+++ pages/11_Телеграм боты с нуля.py
@@ -24,7 +24,7 @@ def convert_backticks_to_bold(text):
     return "".join(parts)
 
 # Основной код страницы
-with open(r"content/11_Телеграм боты (telebot).md", "r", encoding="utf-8") as f:
+with open(r"content/11_Телеграм боты с нуля.md", "r", encoding="utf-8") as f:
     content = f.read()
     
 rendered = convert_backticks_to_bold(content)
