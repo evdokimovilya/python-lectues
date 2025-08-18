9f3a3562f2bdb040ae48de932329097d507fc14d
evdokimovilya <evdokimovilya@yandex.ru>
2025-08-18 14:30:29 +0500

    add orm

--- pages/18_Базы данных. ORM..py
+++ pages/18_Базы данных. ORM..py
@@ -24,7 +24,7 @@ def convert_backticks_to_bold(text):
     return "".join(parts)
 
 # Основной код страницы
-with open(r"content/19_Литература.md", "r", encoding="utf-8") as f:
+with open(r"content/18_Базы данных. ORM..md", "r", encoding="utf-8") as f:
     content = f.read()
     
 rendered = convert_backticks_to_bold(content)
