237c955935af2595dccba351c673b4d596409400
evdokimovilya <evdokimovilya@yandex.ru>
2025-08-29 15:29:17 +0500

    add oop 234

--- pages/13_ООП. Связывание классов.py
+++ pages/13_ООП. Связывание классов.py
@@ -24,7 +24,7 @@ def convert_backticks_to_bold(text):
     return "".join(parts)
 
 # Основной код страницы
-with open(r"content/25_Yandex API.md", "r", encoding="utf-8") as f:
+with open(r"content/13_ООП. Связывание классов.md", "r", encoding="utf-8") as f:
     content = f.read()
     
 rendered = convert_backticks_to_bold(content)
