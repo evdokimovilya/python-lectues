802d3c7b509db4cf2578d8e63f0e52e0b514ad73
evdokimovilya <evdokimovilya@yandex.ru>
2025-09-02 16:00:56 +0500

    add oop, graphic

--- pages/25_Базы данных. ORM.py
+++ pages/25_Базы данных. ORM.py
@@ -24,7 +24,7 @@ def convert_backticks_to_bold(text):
     return "".join(parts)
 
 # Основной код страницы
-with open(r"content/24_Веб. GET запрос. Параметры.md", "r", encoding="utf-8") as f:
+with open(r"content/25_Базы данных. ORM.md", "r", encoding="utf-8") as f:
     content = f.read()
     
 rendered = convert_backticks_to_bold(content)
--- pages/26_Веб. Клиент-сервер. HTTP.py
+++ pages/26_Веб. Клиент-сервер. HTTP.py
@@ -24,7 +24,7 @@ def convert_backticks_to_bold(text):
     return "".join(parts)
 
 # Основной код страницы
-with open(r"content/21_Базы данных. Работа c python.md", "r", encoding="utf-8") as f:
+with open(r"content/26_Веб. Клиент-сервер. HTTP.md", "r", encoding="utf-8") as f:
     content = f.read()
     
 rendered = convert_backticks_to_bold(content)
